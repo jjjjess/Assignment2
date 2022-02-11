8ea5795a30a7d83cfe3a65bf94e8dd29c2f104b9
jesse tripp <tripp.pdf@gmail.com>
2022-02-11 14:45:01 -0500

    2nd commit - 3 points added

--- Assignment 2 - PE 8.7/src/FindNearestPoints.java
+++ Assignment 2 - PE 8.7/src/FindNearestPoints.java
@@ -13,23 +13,26 @@ public class FindNearestPoints {
     int numberOfPoints = input.nextInt();
 
     // Create an array to store points
-    double[][] points = new double[numberOfPoints][2];
+    double[][] points = new double[numberOfPoints][3]; // changed from [2] to [3]
     System.out.print("Enter " + numberOfPoints + " points: ");
     for (int i = 0; i < points.length; i++) {
       points[i][0] = input.nextDouble();
       points[i][1] = input.nextDouble();
+      points[i][2] = input.nextDouble(); // added line for 3rd point
     }
 
     // p1 and p2 are the indices in the points array
     int p1 = 0, p2 = 1; // Initial two points
-    double shortestDistance = distance(points[p1][0], points[p1][1], points[p2][0], points[p2][1]); // Initialize
-                                                                                                    // shortestDistance
+    double shortestDistance = distance(points[p1][0], points[p1][1], points[p1][2], points[p2][0],
+        points[p2][1], points[p2][2]); // Initialize shortest distance - added points[X][2]
+
 
     // Compute distance for every two points
     for (int i = 0; i < points.length; i++) {
       for (int j = i + 1; j < points.length; j++) {
-        double distance = distance(points[i][0], points[i][1], points[j][0], points[j][1]); // Find
-                                                                                            // distance
+        double distance = distance(points[i][0], points[i][1], points[i][2], points[j][0],
+            points[j][1], points[j][2]); // Find distance - added points[X][2]
+
 
         if (shortestDistance > distance) {
           p1 = i; // Update p1
@@ -41,11 +44,13 @@ public class FindNearestPoints {
 
     // Display result
     System.out.println("The closest two points are " + "(" + points[p1][0] + ", " + points[p1][1]
-        + ") and (" + points[p2][0] + ", " + points[p2][1] + ")");
+        + ", " + points[p1][2] + ") and (" + points[p2][0] + ", " + points[p2][1] + ", "
+        + points[p2][2] + ")"); // added points[X][2]
   }
 
   /** Compute the distance between two points (x1, y1) and (x2, y2) */
-  public static double distance(double x1, double y1, double x2, double y2) {
-    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+  // added z1, z2 and updated math
+  public static double distance(double x1, double y1, double z1, double x2, double y2, double z2) {
+    return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)) + ((z2 - z1) * (z2 - z1)));
   }
 }
