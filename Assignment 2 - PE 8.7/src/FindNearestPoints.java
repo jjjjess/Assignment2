1304860a1e95cedd41deb5401c17c8a860a9dc2c
jesse tripp <tripp.pdf@gmail.com>
2022-02-13 16:45:44 -0500

    JUnit testing

--- Assignment 2 - PE 8.7/src/FindNearestPoints.java
+++ Assignment 2 - PE 8.7/src/FindNearestPoints.java
@@ -10,15 +10,16 @@ public class FindNearestPoints {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     double[][] points = inputArray(input);
-    int[] closestIndexes = closestPoints(points);
-    int p1 = closestIndexes[0];
-    int p2 = closestIndexes[1];
+    int[] closestIndices = closestPoints(points);
+    int p1 = closestIndices[0];
+    int p2 = closestIndices[1];
     displayClosestPoints(p1, p2, points);
   }
 
   /** Compute the distance between two points (x1, y1) and (x2, y2) */
   // added z1, z2 and updated math
-  public static double distance(double x1, double y1, double z1, double x2, double y2, double z2) {
+  public static double distanceBetweenPoints(double x1, double y1, double z1, double x2, double y2,
+      double z2) {
     return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)) + ((z2 - z1) * (z2 - z1)));
   }
 
@@ -47,14 +48,15 @@ public class FindNearestPoints {
   public static int[] closestPoints(double[][] points) {
     // p1 and p2 are the indices in the points array
     int p1 = 0, p2 = 1; // Initial two points
-    double shortestDistance = distance(points[p1][0], points[p1][1], points[p1][2], points[p2][0],
-        points[p2][1], points[p2][2]); // Initialize shortest distance - added points[X][2]
+    double shortestDistance = distanceBetweenPoints(points[p1][0], points[p1][1], points[p1][2],
+        points[p2][0], points[p2][1], points[p2][2]); // Initialize shortest distance - added
+                                                      // points[X][2]
 
     // Compute distance for every two points
     for (int i = 0; i < points.length; i++) {
       for (int j = i + 1; j < points.length; j++) {
-        double distance = distance(points[i][0], points[i][1], points[i][2], points[j][0],
-            points[j][1], points[j][2]); // Find distance - added points[X][2]
+        double distance = distanceBetweenPoints(points[i][0], points[i][1], points[i][2],
+            points[j][0], points[j][1], points[j][2]); // Find distance - added points[X][2]
 
         if (shortestDistance > distance) {
           p1 = i; // Update p1
